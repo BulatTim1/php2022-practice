d22903bb38480207b577e12fce18d59f16272eb0
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2022-07-19 01:44:03 +0300

    Delete last_practise directory

--- last_practise/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'include/scripts.php';
-include 'include/Database.php';
-
-include 'templates/header.php';
-?>
-<h1>Главная страница</h1>
-<?php include 'templates/footer.php'; ?>
--- last_practise/post.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'include/Database.php';
-if (isset($_POST['title']) && isset($_POST['body'])) {
-    $title = $_POST['title'];
-    $body = $_POST['body'];
-    DB::addPost($title, $body);
-}
-
-header('Location: '.$_SERVER['HTTP_REFERER']);
